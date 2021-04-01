4687d7fa9879ed655bbbcd5a7a02bf1d91136ea9
unknown <zxd41082@gmail.com>
2021-04-01 21:15:50 +0800

    header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <global-header :user="currentUser"></global-header>
+    <div class="home"></div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import GlobalHeader  from '../components/GlobalHeader.vue'
+interface UserProps {
+  isLogin: boolean;
+  name?: string;
+  id?: number;
+}
+const currentUser: UserProps = {
+    isLogin: false,
+}
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld,
+    GlobalHeader
   },
+  setup() {
+    return {
+      currentUser
+    }
+  }
 });
 </script>
